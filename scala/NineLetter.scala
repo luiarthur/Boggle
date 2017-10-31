7b15f450eab9d552b8f0de5980325cbc01e980d1
Arthur Lui <luiarthur@gmail.com>
2017-10-30 22:00:32 -0700

    yes

--- scala/NineLetter.scala
+++ scala/NineLetter.scala
@@ -3,7 +3,8 @@ import scala.util.Random.shuffle
 // NOT DONE. TODO
 
 object NineLetter {
-  private val dictionary = io.Source.fromFile("dictionary.txt").getLines.toList
+  //private val dictionary = io.Source.fromFile("dictionary.txt").getLines.toList
+  private val dictionary = io.Source.fromFile("big_dict.txt").getLines.toList
 
   def isValidWord(w:String) = dictionary.contains(w)
   def candidates(w:String) = {
@@ -15,30 +16,41 @@ object NineLetter {
     a + b.tail
   }
 
-  def search(curr:String, remains:String, words:List[String]):List[String] = {
-    if (remains == "" || remains > ""  && !isPotentialWord(curr)) {
-      if (isValidWord(curr)) curr :: words else words
-    } else  {
-      val idxLeft = List.range(0, remains.size)
-      idxLeft.par.flatMap{i => 
-        val newWords = if (isValidWord(curr)) curr :: words else words
-        search(curr + remains(i), removeAt(remains, i), newWords)
-      }.distinct.toList
+
+  def find(input:String) = {
+    //val words = collection.mutable.Set[String]()
+    //val visited = collection.mutable.Set[String]()
+
+    def search(curr:String, remains:String, words:List[String]):List[String] = {
+      if (remains == "" || remains > ""  && !isPotentialWord(curr)) {
+        if (isValidWord(curr)) curr :: words else words
+      } else  {
+        val idxLeft = List.range(0, remains.size)
+        idxLeft.par.flatMap{i => 
+          val newWords = if (isValidWord(curr)) curr :: words else words
+          search(curr + remains(i), removeAt(remains, i), newWords)
+        }.distinct.toList
+      }
     }
+
+    search("", input, List()).distinct.sorted
   }
 
 
   def main(args:Array[String]) {
-    val word = args(0)
-    val words = search("", word, List()).toSet
-    words.foreach(println)
+    val input = args(0)
+    println("Here is the input: " + input)
+    val words = find(input)
+    println("Here is the result:")
+    words.filter(_.size == input.size).foreach(println)
   }
 }
 
 // Test
-//val letters = "theaters"
-val letters = "robbery"
-val input = shuffle(letters.toList).mkString
-val result = NineLetter.search("", input, List()).distinct.sorted
+//val letters = "unamusingly"
+//val letters = "robbery"
+//val letters = "nose"
+//val input = shuffle(letters.toList).mkString
+//val result = NineLetter.find(input)
 
-result.filter(_.size == 6)
+//result.filter(_.size == letters.size)
