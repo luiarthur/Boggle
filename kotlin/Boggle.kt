5aedac8fc6667896424413a94040b3421d0f0cf5
Arthur Lui <luiarthur@gmail.com>
2020-01-08 12:59:16 -0700

    yes

--- kotlin/Boggle.kt
+++ kotlin/Boggle.kt
@@ -1,12 +1,39 @@
-class Die(val faces: List<String>) {
+import java.util.Random
+
+class Die(vararg val faces: String) {
+  val numFaces = faces.size
+
   fun roll(): String {
-    return this.faces.shuffled()[0]
+    val idx = Random().nextInt(numFaces)
+    return this.faces[idx]
   }
 }
 
+/* Test
+val d = Die("A", "B")
+d.roll()
+ */
 
-// class BoggleBoard(val dice: Array<Array<Die>>) {
-//   fun shuffled(): BoggleBoard {
-//     // BoggleBoard()
-//   }
-// }
+
+class BoggleBoard(val dice: Array<Array<Die>>) {
+  val rows = dice.size
+  val cols = dice[0].size
+
+  // TODO: Assert that number of columns is the same every row.
+
+  fun shuffled(): Array<Array<String>> {
+    val newDiceFlat = dice.flatten().shuffled()
+    val newDice = Array(rows) {
+      r -> Array(cols) {
+        c -> newDiceFlat[r * c + c].roll()
+      }
+    }
+
+    return newDice
+  }
+}
+
+// Test
+val BB = BoggleBoard(arrayOf(
+    arrayOf(Die("A", "B"), Die("C", "D")),
+    arrayOf(Die("X", "Y"), Die("W", "U"))))
