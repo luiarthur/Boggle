a111adb2386d2d2f11f7773013953a5705fd4d77
Arthur Lui <luiarthur@gmail.com>
2020-01-10 14:30:35 -0700

    making board

--- BoggleScalaJS/src/main/scala/Boggle/Board.scala
+++ BoggleScalaJS/src/main/scala/Boggle/Board.scala
@@ -3,7 +3,23 @@ package Boggle
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 import scala.scalajs.js
 import scala.util.Random
+import Helper.isSquare
 
+@JSExportTopLevel("Board")
 case class Board(dice: js.Array[Die],
                  dictionary: js.Array[String],
-                 minLetters: Int=3)
+                 minLetters: Int=3) {
+  type Matrix = Array[Array[String]]
+
+  val numDice = dice.size
+  require(isSquare(numDice))
+
+  val rows = math.sqrt(numDice).toInt
+  val cols = rows
+  var letters = Array.ofDim[String](rows, cols)
+
+  private def shuffled(): Matrix = {
+    ???
+  }
+
+}
