5dafe0c154d62b3a5c30a7be9f20c483652bd422
Arthur Lui <luiarthur@gmail.com>
2020-01-10 00:38:19 -0700

    js boggle ?

--- kotlin-js/dev/src/main/kotlin/BoggleJS/Boggle.kt
+++ kotlin-js/dev/src/main/kotlin/BoggleJS/Boggle.kt
@@ -1,16 +1,12 @@
 package BoggleJS
 
-
-fun nextInt(n: Int): Int {
-  return Math.floor(Math.random() * n).toInt()
-}
-
+import kotlin.random.Random
 
 class Die(vararg val faces: String) {
   val numFaces = faces.size
 
   fun roll(): String {
-    val idx = nextInt(numFaces)
+    val idx = Random.nextInt(numFaces)
     return faces[idx]
   }
 }
